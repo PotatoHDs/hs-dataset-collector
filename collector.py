547d570625aaea1da97f7d22305c5bc02cdc34a4
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2021-10-24 21:57:54 +0300

    It's working (finally)

--- collector.py
+++ collector.py
@@ -6,86 +6,82 @@ import aiofiles as aiof
 from aiohttp.client_exceptions import ClientOSError, ServerDisconnectedError
 
 
-async def fetch(game_id, session):
-    url = f'https://hsreplaynet-replays.s3.amazonaws.com/viewed_replays/{game_id}.hsreplay.xml'
-    res = ''
-    while not res:
+async def fetch(game_id, session, sem):
+    async with sem:
+        res = ''
+        while not res:
+            try:
+                async with session.get(f'https://hsreplay.net/api/v1/games/{game_id}/', timeout=5) as response:
+                    print(f'Fetched game link {game_id}')
+                    link = (await response.json())['replay_xml']
+                async with session.get(link, timeout=5) as response:
+                    print(f'Fetched game {game_id}')
+                    res = await response.text()
+            except (ClientOSError, ServerDisconnectedError, asyncio.TimeoutError, KeyError) as e:
+                if type(e) == asyncio.TimeoutError:
+                    print('Timeout error')
+                else:
+                    print(e)
+        return res
+
+
+async def write_file(data, path):
+    print(f'Writing to {path}')
+    done = False
+    while not done:
         try:
-            async with session.get(url, timeout=10) as response:
-                print(f'Fetched game {game_id}')
-                res = await response.text()
-        except (ClientOSError, ServerDisconnectedError, asyncio.TimeoutError) as e:
-            if type(e) == asyncio.TimeoutError:
-                print('Timeout error')
-            else:
-                print(e)
-    return res
-
-
-# async def write_file(data, path):
-#     print(f'Writing to {path}')
-#     done = False
-#     while not done:
-#         try:
-#             async with aiof.open(path, "w") as out:
-#                 await out.write(json.dumps(data))
-#                 done = True
-#         except (ConnectionResetError, OSError) as e:
-#             print(e)
+            async with aiof.open(path, "w") as out:
+                await out.write(json.dumps(data))
+                done = True
+        except Exception as e:
+            # (ConnectionResetError, OSError)
+            print(e, type(e))
 
 
 async def main():
     tasks = []
-    count = 0
+    # count = 0
     # create instance of Semaphore
     # Create client session that will ensure we dont open new connection
     # per each request.
     async with ClientSession() as session:
-        async with session.get('https://hsreplay.net/api/v1/live/replay_feed/') as response:
-            prev = set()
-            while True:
-                start = time.time()
+        prev = set()
+        sem = asyncio.Semaphore(200)
+        while True:
+            start = time.time()
+            async with session.get('https://hsreplay.net/api/v1/live/replay_feed/') as response:
                 games = (await response.json())["data"]
                 print('Collected games')
-                if all(game['id'] not in prev for game in games):
+                # print(prev, {game['id'] for game in games}, sep='\n')
+                if prev != {game['id'] for game in games}:
                     for result in games:
-                        task = asyncio.ensure_future(fetch(result['id'], session))
+                        task = asyncio.ensure_future(fetch(result['id'], session, sem))
                         tasks.append(task)
                     responses = await asyncio.gather(*tasks)
                     print('Fetched all games')
+                    tasks = []
+                    prev = set()
                     for i in range(len(games)):
                         if responses[i]:
                             prev.add(games[i]['id'])
                             games[i]['xml'] = responses[i]
-                            async with open(fr'D:\datasets and shit\hs_games\{games[i]["id"]}', "w") as out:
-                                await out.write(json.dumps(games[i]))
-                            # task = asyncio.ensure_future(
-                            #     write_file(games[i], fr'D:\datasets and shit\hs_games\{games[i]["id"]}'))
-                            # tasks.append(task)
-                            count += 1
+                            task = asyncio.ensure_future(
+                                write_file(games[i], fr'D:\datasets and shit\hs_games\{games[i]["id"]}.json'))
+                            tasks.append(task)
+                            # count += 1
+                    await asyncio.gather(*tasks)
                     tasks = []
-                    prev = set()
 
-                end = time.time() - start
-                if 30 - end > 0:
-                    await asyncio.sleep(30 - end)
-                print(count)
-            # print(type(res))
-
-        # for i in range(1, 600):
-        #     # pass Semaphore and session to every GET request
-        #     task = asyncio.ensure_future(fetch(f'https://pokeapi.co/api/v2/pokemon/{i}', session))
-        #     tasks.append(task)
-        # responses = asyncio.gather(*tasks)
-        # res = await responses
-        # print(len(res))
+            end = time.time() - start
+            if 30 - end > 0:
+                await asyncio.sleep(30 - end)
+            # print(count)
 
 
 if __name__ == "__main__":
     start_time = time.time()
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-    # asyncio.run(main())
     print("--- %s seconds ---" % (time.time() - start_time))
 
 # https://hsreplay.net/api/v1/live/replay_feed/
